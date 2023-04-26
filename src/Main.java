1686891a591002acdbdf368ca4d8f31f8ac299f4
frank valle <fvalle81@gmail.com>
2023-04-25 22:29:35 -0500

    Estructura de metodos para la bd. modificación1.1

--- src/Main.java
+++ src/Main.java
@@ -67,7 +67,7 @@ public class Main {
     // Harumy
     static void buscaPostulante() {
     }
-    //QueryDB método nuevo por frank valle sanchez
+    //QueryDB método nuevo por frank C. valle sanchez
     static void queryDB(String type, String[] data) {
         switch (type){
             case "insert" : //table_postulantes
